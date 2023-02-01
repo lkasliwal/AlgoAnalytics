0f68c8e80b0449378473447b2d96afcb1aae4ba3
NoobCoder312 <hrajput@algoanalytics.com>
2023-02-01 10:12:28 +0530

    utils files

---
+++ utils/mail.js
@@ -0,0 +1,35 @@
+
+const nodemailer = require('nodemailer')
+const crypto=require('crypto');
+const sendError = require('./helper');
+
+
+const generateTranporter=()=>{
+    var transport = nodemailer.createTransport({
+        host: "smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+          user: "harshalrajput312@GMAIL.COM",
+          pass: "legbrnxgxemrebha"
+        }
+      });
+}
+
+const generateRandomByte = () => {
+  return new Promise((resolve, reject) => {
+      crypto.randomBytes(30, (err, buff) => {
+          if (err) reject(err);
+          const buffString = buff.toString("hex");
+
+          resolve(buffString);
+      });
+  });
+};
+
+const notFoundRoute=function(req,res){
+ // sendError()
+this.sendError(res,'Route not found',404)
+}
+
+
+module.exports={generateRandomByte,notFoundRoute}
