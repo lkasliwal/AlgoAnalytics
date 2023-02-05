68cdc2276b99692b39bbf32893ee293cbfcab24f
Prathamesh Rathi <prathameshrathi14@gmail.com>
2023-02-05 17:26:02 +0530

    Checkout other branch

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express=require('express')
 const app=express()
 require('./dbConnection/mongoose')
 const userRouter =require('./routes/user')
-const partRouter = require('./routes/part');
+const operatorRouter = require('./routes/operator');
 const adminRouter = require('./routes/admin')
+const cors = require('cors')
+
+app.use(cors())
 app.use(express.json())
 app.use(userRouter);
-app.use(partRouter);
+app.use(operatorRouter);
 app.use(adminRouter);
 
 app.listen(4000,()=>{
