b670912f7de727ad35808bc3d933b2139dc60fd0
Lokesh Kasliwal <kasliwallokesh@gmail.com>
2023-03-02 09:30:23 +0530

    Updated select part api and sending part name to django

--- routes/part.js
+++ routes/part.js
@@ -1,10 +1,24 @@
 const express=require('express')
 const Router=express.Router()
 const User = require('../models/user')
+const axios = require('axios');
+const apiEndpoint = process.env.Select_Part_API_ENDPOINT;
 
 Router.get('/api/operator/selectpart',async(req,res)=>{
     try{
         const part_name = req.body.part_name;
+        const data = {
+            name: part_name
+          };
+        axios.get(apiEndpoint,{params:data})
+        .then(response => {
+            // part_ok_not_ok=response.data
+            // console.log(response.data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
         const part = await Part.findOne({part_name});
         res.status(200).send(part);
     }
