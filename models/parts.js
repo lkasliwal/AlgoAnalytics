9e1bef6fb42f52a643aff02ec2a60f086cbc1fd7
pendserohit <rpendse@algoanalytics.com>
2023-03-21 14:44:48 +0530

    part_details added in part_schema

--- models/parts.js
+++ models/parts.js
@@ -1,33 +1,25 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
-const partSchema=mongoose.Schema({
-    part_name:{
-      type:String,
-      required:true,
-      trim:true
-    },
-    part_ok:{
-     type:Number,
-     required:true,
-     
-    },
-    part_not_ok:{
-      type:Number,
-      required:true,
-      
-    },
-    precision:{
-      type: Number,
-      required:true,
-    },
-    recall: {
-      type: Number,
-      default: 0
-    },
-    map:{
-      type: Number,
-      default: 0
+const partSchema = mongoose.Schema({
+  part_name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  part_ok: {
+    type: Number,
+    required: true,
+
+  },
+  part_not_ok: {
+    type: Number,
+    required: true,
+  },
+  part_details: {
+    part_date: {
+      type: Date,
     }
- })
+  }
+})
 
- module.exports = mongoose.model('Part',partSchema)
+module.exports = mongoose.model('Part', partSchema)
