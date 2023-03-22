68a72fc43a5ba7b7144d0d8845abf2c55480be46
pendserohit <rpendse@algoanalytics.com>
2023-03-22 14:13:50 +0530

    Constants

---
+++ constants/constants.js
@@ -0,0 +1,11 @@
+class Constants {
+    static part_ok = 0;
+    static part_not_ok = 0;
+
+    Constants() {
+        part_ok = 0;
+        part_not_ok = 0;
+    }
+}
+
+module.exports = Constants;
